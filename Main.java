3fb92064699a3a9ef1788a5e966d12de30b8b4c0
Viktor Surzhko <wiktor_vitos@mail.ru>
2018-12-24 19:32:24 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Shape circle = new Circle();
+	Shape triangle = new Triangle();
+	Shape square = new Square();
+	circle.draw();
+	triangle.draw();
+	square.draw();
+    }
+}
